41524600fa6ba7ac73ce94bed6658372241cde46
Daniel Kirkdorffer <dankirkd@comcast.net>
2011-02-22 22:26:24 -0800

    Baseline version of ribbet application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ribbet::Application.config.secret_token = 'e1d4da288389bca4fd0e3e247d5ab1556387621b768c81f8296800bd9198ba903fcd5c3e37988741ea3679091ebe75afd941b3d26c548452ca0db99f89d0908a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ribbet::Application.config.session_store :cookie_store, :key => '_ribbet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ribbet::Application.config.session_store :active_record_store
