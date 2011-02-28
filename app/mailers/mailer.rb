dd518ec39783bd38e8911a591730b8d6b20dea18
Daniel Kirkdorffer <dankirkd@comcast.net>
2011-02-27 17:36:40 -0800

    Minor change.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,6 +2,7 @@ class Mailer < Devise::Mailer
 
   def reset_password_instructions(record)
     attachments.inline['jumpingfrog.jpg'] = File.read(Rails.root.join('public/images/jumpingfrog.gif'))
+    # Call up to Devise::Mailer method:
     setup_mail(record, :reset_password_instructions)
   end
 
