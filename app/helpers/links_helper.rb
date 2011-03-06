d4f8ed2bb0bf87b12cb26eeb37ccadd591d4d894
Daniel Kirkdorffer <dankirkd@comcast.net>
2011-03-06 02:14:12 -0800

    Added new logic to display alternate thumb gifs if a vote has been cast already.

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,4 +2,37 @@ module LinksHelper
   def votecount(link)
     Vote.sum(:total, :conditions => ['link_id = ?', link.id])
   end
+  
+  def votecast(link)
+    @vote = Vote.where("votes.link_id = ? AND votes.email = ?", link.id, current_user.email).first
+    if @vote.nil?
+      return 0
+    else
+      return @vote.total
+    end
+  end
+  
+  def getthumbimage(type, link)
+    votecast_result = votecast(link)
+    if (type == "up" && votecast_result == 1)
+      return "thumbs" + type + "cast.gif"
+    elsif (type == "down" && votecast_result == -1) 
+      return "thumbs" + type + "cast.gif"
+    else
+      return "thumbs" + type + ".gif"
+    end
+  end
+  
+  def getthumbtext(type, link)
+    votecast_result = votecast(link)
+    if (type == "up" && votecast_result == 1)
+      return "You voted for this link"
+    elsif (type == "down" && votecast_result == -1) 
+      return "You voted against this link"
+    elsif (type == "up" && votecast_result <= 0) 
+      return "Vote for this link"
+    elsif (type == "down" && votecast_result >= 0) 
+      return "Vote against this link"
+    end
+  end
 end
